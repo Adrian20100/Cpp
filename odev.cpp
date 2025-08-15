f9ed91bcf72f0d631fd8209768c59a6e88b753c7
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-16 00:34:18 +0200

    Create odev.cpp
    Find out if a number is even or odd

---
+++ odev.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int myNum = 8;
+    if (myNum % 2 == 0)
+    {
+        cout << myNum << " is even. \n";
+    }
+    else
+    {
+        cout << myNum << " is odd. \n";
+    }
+    return 0;
+}
