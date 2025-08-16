14111f5eeafdfdfcde01a5b39fd0ad434d254157
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-16 13:53:31 +0200

    the multiplication table for a specified number

---
+++ multiplicationtable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int number = 2;
+    int i;
+
+    for (i = 1; i <= 10; i++)
+    {
+        cout << number << " x " << i << " = " << number * i << "\n";
+    }
+
+    return 0;
+}
