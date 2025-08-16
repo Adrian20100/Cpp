bb5ce2bb153cc5b7532590817c8b25c611f83838
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-16 13:50:50 +0200

    powers of 2 up to 512 loop

---
+++ pwrloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    for (int i = 2; i <= 512; i *= 2)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
