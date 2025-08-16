2374bad50a7fca72576d7708598c1820ab9e289a
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-16 13:45:32 +0200

    even numbers between 0 and 10 (inclusive)

---
+++ loopeven.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <= 10; i += 2)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
