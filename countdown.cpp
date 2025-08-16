13b8d4b2101c8045e6ba1e59aa109002b88b543d
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-16 12:19:40 +0200

    a simple "countdown" program

---
+++ countdown.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int countdown = 3;
+
+    while (countdown > 0)
+    {
+        cout << countdown << "\n";
+        countdown--;
+    }
+    cout << "Happy New Year!!\n";
+
+    return 0;
+}
