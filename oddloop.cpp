0cba99fd0eb11169aba95b6d46e428df6f5b38d1
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-16 13:46:34 +0200

    print odd numbers loop

---
+++ oddloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i += 2)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
