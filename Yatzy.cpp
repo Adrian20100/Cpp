56329d3d492e769872ed41bd53acfcd794fcd17b
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-16 12:56:39 +0200

    a practical example of the while loop combined with an if else statement

---
+++ Yatzy.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int dice = 1;
+
+    while (dice <= 6)
+    {
+        if (dice < 6)
+        {
+            cout << "No Yatzy\n";
+        }
+        else
+        {
+            cout << "Yatzy!\n";
+        }
+        dice = dice + 1;
+    }
+}
