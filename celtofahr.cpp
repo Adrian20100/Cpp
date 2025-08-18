588b3c74d12da50924da1d5045ad5441c57d86fa
Adrian20100 <131767843+Adrian20100@users.noreply.github.com>
2025-08-18 12:02:21 +0200

    a program that converts a value from fahrenheit to celsius

---
+++ celtofahr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+float toCelsius(float fahrenheit)
+{
+    return (5.0 / 9.0) * (fahrenheit - 32.0);
+}
+
+int main()
+{
+    float f_value = 98.8;
+
+    float result = toCelsius(f_value);
+
+    cout << "Fahrenheit: " << f_value << "\n";
+
+    cout << "Convert Fahrenheit to Celsius: " << result << "\n";
+
+    return 0;
+}
